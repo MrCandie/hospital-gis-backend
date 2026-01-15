dd7482acd6e9d4f807aa8ba9e25e7f324ee70cca
mrcandie <akinwumiadekanmi2@gmail.com>
2026-01-15 13:34:09 +0100

    postgis migration

--- config/database.js
+++ config/database.js
@@ -3,29 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = !process.env.NODE_ENV === "production";
 
-const sequelize = isProduction
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-    })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-        logging: false,
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+// : new Sequelize(process.env.DATABASE_URL, {
+//     dialect: "postgres",
+//     logging: false,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   });
 
 export default sequelize;
